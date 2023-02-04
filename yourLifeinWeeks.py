4a8bcfee93f9da4bc6865ea7b310a0e188562e62
hakan <hakanatak@gmail.com>
2023-02-04 16:49:43 +0300

    Create yourLifeinWeeks.py

---
+++ yourLifeinWeeks.py
@@ -0,0 +1,8 @@
+age = input("What is your cuurent age ?")
+age_int = int(age)
+years_remaining = 90-age_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks and {months_remaining} months, get a happy life !")
