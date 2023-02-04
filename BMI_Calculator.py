9da46f71eafe586cae3cf758e381667474e5ef28
hakan <hakanatak@gmail.com>
2023-02-04 16:28:19 +0300

    Update BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -3,4 +3,4 @@ weight = input("Enter your weight in kg: ")
 
 bmi = float(weight) / float(height) ** 2
 bmi_int = int(bmi)
-print(f"Your height is {height} m, your weight is {weight} kg and Your BMI is {bmi_int} .")
+print(f"Your height is {height} m, your weight is {weight} kg and Your BMI score is {bmi_int} .")
